1e0592d0a671f6646b28a767466eeb1b3bf6f617
penglei <yupengniaosanshi@gmail.com>
2018-05-01 17:49:37 +0800

    网络类添加常量

--- app/src/main/java/com/example/penglei/udasunshine/utilities/NetworkUtils.java
+++ app/src/main/java/com/example/penglei/udasunshine/utilities/NetworkUtils.java
@@ -9,6 +9,30 @@ import java.util.Scanner;
  * 网络请求类
  */
 public class NetworkUtils {
+    /**
+     * 网络地址常量
+     */
+    private static final String DYNAMIC_WEATHER_URL = "https://andfun-weather.udacity.com/weather";
+    private static final String STATIC_WEATHER_URL = "https://andfun-weather.udacity.com/staticweather";
+
+    private static final String FORECAST_BASE_URL = STATIC_WEATHER_URL;
+
+    /**
+     * 参数配置
+     */
+    private static final String format = "json";
+    private static final String units = "metric";
+    private static final int numDays = 14;
+
+    /**
+     * 参数名列表
+     */
+    static final String QUERY_PARAM = "q";
+    static final String LAT_PARAM = "lat";
+    static final String LON_PARAM = "lon";
+    static final String FORMAT_PARAM = "mode";
+    static final String UNITS_PARAM = "units";
+    static final String DAYS_PARAM = "cnt";
 
     /**
      * 构建URL
